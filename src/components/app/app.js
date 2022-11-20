770cf95423011e6d71600f8fb078e1634407f1ce
vlad_nolimits <start2top@gmail.com>
2022-11-21 03:52:34 +0500

    added searching panel and posts filters

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -15,12 +15,17 @@ export default class App extends Component {
                 {label: 'Going to learn react', important: true, like: false, id: 1},
                 {label: 'That is so good', important: false, like: false, id: 2},
                 {label: 'I need a break...', important: false, like: false, id: 3}
-            ]
+            ],
+            term: '',
+            filter: 'all'
         };
         this.deleteItem = this.deleteItem.bind(this);
         this.addItem = this.addItem.bind(this);
         this.onToggleImportant = this.onToggleImportant.bind(this);
         this.onToggleLiked = this.onToggleLiked.bind(this);
+        this.onUpdateSeach = this.onUpdateSeach.bind(this);
+        this.onFilterSelect = this.onFilterSelect.bind(this);
+        
         this.maxId = 4;
     }
 
@@ -81,22 +86,53 @@ export default class App extends Component {
         })
     }
 
+    searchPost(items, term) {
+        if (term.length === 0) {
+            return items
+        }
+
+        return items.filter( (item) => {
+            return item.label.indexOf(term) > -1
+        })
+    }
+
+    filterPost(items, filter) {
+        if (filter === 'like') {
+            return items.filter(item => item.like)
+        } else {
+            return items
+        }
+    }
+
+    onUpdateSeach(term) {
+        this.setState({term})
+    }
+
+    onFilterSelect(filter) {
+        this.setState({filter})
+    }
+
     render() {
-        const {data} = this.state;
+        const {data, term, filter } = this.state;
         const liked = data.filter(item => item.like).length;
         const allPosts = data.length;
         
+        const visiblePosts = this.filterPost(this.searchPost(data, term) , filter);
         return (
             <div className="app">
                 <AppHeader
                     liked = {liked}
                     allPosts = {allPosts}/>
                 <div className="search-panel d-flex">
-                    <SearchPanel/>
-                    <PostStatusFilter/>
+                    <SearchPanel
+                        onUpdateSeach={this.onUpdateSeach}
+                        />
+                    <PostStatusFilter
+                    filter={filter}
+                    onFilterSelect={this.onFilterSelect}/>
                 </div>
             <PostList 
-                    posts={this.state.data}
+                    posts={visiblePosts}
                     onDelete={this.deleteItem}
                     onToggleImportant={this.onToggleImportant}
                     onToggleLiked={this.onToggleLiked}/>
--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const SearchPanel = () => {
+export default class SearchPanel extends Component  {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: ''
+        }
+        this.onUpdateSeach = this.onUpdateSeach.bind(this);
+    }
+    onUpdateSeach(e) {
+        const term = e.target.value;
+        this.setState({term});
+        this.props.onUpdateSeach(term);
+    }
+   render() {
     return (
         <input 
             className="form-control search-input"
-            type="text" placeholder="Поиск по записям"
+            type="text" 
+            placeholder="Поиск по записям"
+            onChange={this.onUpdateSeach}
         />
     )
+   } 
+    
 }
-
-export default SearchPanel;
